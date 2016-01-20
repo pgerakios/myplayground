bcd4e13a4b3613a3c9f2f1023cfedf66221a8a26
Prodromos Gerakios <p.gerakios@afse.eu>
2016-01-20 16:47:37 +0200

    Rest express framework sample app

---
+++ lightweightWebApp/java/rest-express-sample/src/main/java/com/github/gerakios/restexpress/sample/Main.java
@@ -0,0 +1,35 @@
+package com.github.gerakios.restexpress.sample;
+
+import java.io.IOException;
+
+import org.restexpress.RestExpress;
+import org.restexpress.pipeline.SimpleConsoleLogMessageObserver;
+import com.github.gerakios.restexpress.sample.serialization.SerializationProvider;
+import org.restexpress.util.Environment;
+
+public class Main
+{
+	private static final String SERVICE_NAME = "TODO: Enter service name";
+
+	public static void main(String[] args) throws Exception
+	{
+		RestExpress server = initializeServer(args);
+		server.awaitShutdown();
+	}
+
+	public static RestExpress initializeServer(String[] args) throws IOException
+	{
+		RestExpress.setSerializationProvider(new SerializationProvider());
+
+		Configuration config = Environment.load(args, Configuration.class);
+		RestExpress server = new RestExpress()
+				.setName(SERVICE_NAME)
+				.setBaseUrl(config.getBaseUrl())
+				.setExecutorThreadCount(config.getExecutorThreadPoolSize())
+				.addMessageObserver(new SimpleConsoleLogMessageObserver());
+
+		Routes.define(config, server);
+		server.bind(config.getPort());
+		return server;
+    }
+}
---
+++ lightweightWebApp/java/rest-express-sample/src/main/java/com/github/gerakios/restexpress/sample/serialization/XmlSerializationProcessor.java
@@ -0,0 +1,16 @@
+package com.github.gerakios.restexpress.sample.serialization;
+
+import org.restexpress.serialization.xml.XstreamXmlProcessor;
+
+public class XmlSerializationProcessor
+extends XstreamXmlProcessor
+{
+	public XmlSerializationProcessor()
+    {
+	    super();
+//		alias("element_name", Element.class);
+//		alias("element_name", Element.class);
+//		alias("element_name", Element.class);
+//		alias("element_name", Element.class);
+    }
+}
